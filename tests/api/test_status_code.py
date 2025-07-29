58c324496204411dd6ed6886da75e9529def5f8b
Aleksei <67508451+lifemonitors@users.noreply.github.com>
2025-07-29 18:39:14 -0500

    test_status_code.py

--- tests/api/test_status_code.py
+++ tests/api/test_status_code.py
@@ -1,5 +1,10 @@
 import requests
+import pytest
 
-def test_homepage_status():
-    response = requests.get("https://www.saucedemo.com")
+@pytest.mark.parametrize("url", [
+    "https://example.com",
+    "https://httpbin.org/status/200"
+])
+def test_status_code(url):
+    response = requests.get(url)
     assert response.status_code == 200
